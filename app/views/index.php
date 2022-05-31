14a625e400203ab02d0321c1a578fe8a3da661c8
chau.pham@platform.inc <chau.pham@platform.inc>
2022-05-31 21:32:21 +0700

    design tour

--- app/views/index.php
+++ app/views/index.php
@@ -7,32 +7,55 @@
 <div id="index-container">
 
     <div class="index-image">
-       <div class="index-content">
-           <h1>Du lich theo phong cach rieng</h1>
-           <h3>Trai nghiem tron ven - Gia ca phai chang.</h3>
-       </div>
-       <form action="" method="get">
-           <div class="add-to">
-                <i class="fas fa-map-marker-alt"></i>
-                <input type="text" name="add_to" id="add_to" placeholder="Ban muon di dau">
-           </div>
-            <div class="date-time">
-                <div class="time">
-                    <i class=""></i>
-                    <input type="date" name="date" id="date">
-                </div>
-                <div class="add_from">
-                    <i class=""></i>
-                    <section id="add_from">
-                        <option value="1">Ha Noi</option>
-                        <option value="2">Ho Chi Minh</option>
-                        <option value="3"></option>
-                        <option value="4"></option>
-                        <option value="5"></option>
-                        <option value="6"></option>
-                    </section>
+        <div class="index-body">
+            <div class="index-content">
+                <h1>Du lich theo phong cach rieng</h1>
+                <h3>Trai nghiem tron ven - Gia ca phai chang.</h3>
+            </div>
+            <div class="index-form">
+                <form action="" method="get">
+                    <div class="add-to">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <input type="text" name="add_to" id="add_to" placeholder="Ban muon di dau?">
+                    </div>
+                    <div class="date-time">
+                        <div class="time">
+                            <i class=""></i>
+                            <input type="date" name="date" id="date">
+                        </div>
+                        <div class="add_from">
+                            <i class="fas fa-plane"></i>
+                            <input type="text" name="add_from" id="add_from" placeholder="Khoi hanh tu">
+                        </div>
+                        <div class="form-button">
+                            <input type="button" value="Tim">
+                        </div>
+                    </div>
+                </form>
+                <div class="content-form">
+                    <p>Tailor tour - Trai nghiem theo cach cua ban</p>
+                    <input type="button" value="Dat tour rieng">
                 </div>
             </div>
-       </form>
+        </div>
+    </div>
+    <div class="index-select">
+        <div class="select-item">
+            <i class="fa fa-check-circle"></i>
+            <p>Gia tour tot nhat</p>
+        </div>
+        <div class="select-item">
+            <i class="fa fa-check-circle"></i>
+            <p>Gia tour tot nhat</p>
+        </div>
+        <div class="select-item">
+            <i class="fa fa-check-circle"></i>
+            <p>Gia tour tot nhat</p>
+        </div>
+        <div class="select-item">
+            <i class="fa fa-check-circle"></i>
+            <p>Gia tour tot nhat</p>
+        </div>
+
     </div>
 </div>
