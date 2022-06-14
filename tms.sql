8e8df3496817a72065f476cf85347792cde3e572
Chauptm <nobitachauphong@gmail.com>
2022-06-15 02:01:06 +0700

    add db

--- tms.sql
+++ tms.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 14, 2017 at 10:26 AM
--- Server version: 5.6.26
--- PHP Version: 5.5.28
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th6 14, 2022 lúc 08:57 PM
+-- Phiên bản máy phục vụ: 10.4.22-MariaDB
+-- Phiên bản PHP: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,126 +18,113 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tms`
+-- Cơ sở dữ liệu: `tms`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Cấu trúc bảng cho bảng `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `UserName` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `Role` int not null,
-  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `Role` int(11) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `admin`
+-- Đang đổ dữ liệu cho bảng `admin`
 --
 
-INSERT INTO `admin` (`id`, `UserName`, `Password`,`Role`, `updationDate`) VALUES
-(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e','1', '2022-06-01 11:18:49');
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `Role`, `updationDate`) VALUES
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 1, '2022-06-01 11:18:49'),
+(2, 'admintour', 'e10adc3949ba59abbe56e057f20f883e', 2, '2022-06-12 07:39:53'),
+(3, 'guide', 'e10adc3949ba59abbe56e057f20f883e', 3, '2022-06-12 07:40:30');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblbooking`
+-- Cấu trúc bảng cho bảng `tblbooking`
 --
 
-CREATE TABLE IF NOT EXISTS `tblbooking` (
+CREATE TABLE `tblbooking` (
   `BookingId` int(11) NOT NULL,
   `PackageId` int(11) NOT NULL,
   `UserEmail` varchar(100) NOT NULL,
   `FromDate` varchar(100) NOT NULL,
   `ToDate` varchar(100) NOT NULL,
   `Comment` mediumtext NOT NULL,
-  `RegDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` int(11) NOT NULL,
   `CancelledBy` varchar(5) DEFAULT NULL,
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblbooking`
---
-
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblenquiry`
+-- Cấu trúc bảng cho bảng `tblenquiry`
 --
 
-CREATE TABLE IF NOT EXISTS `tblenquiry` (
+CREATE TABLE `tblenquiry` (
   `id` int(11) NOT NULL,
   `FullName` varchar(100) NOT NULL,
   `EmailId` varchar(100) NOT NULL,
   `MobileNumber` char(10) NOT NULL,
   `Subject` varchar(100) NOT NULL,
   `Description` mediumtext NOT NULL,
-  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
   `Status` int(1) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblenquiry`
---
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblissues`
+-- Cấu trúc bảng cho bảng `tblissues`
 --
 
-CREATE TABLE IF NOT EXISTS `tblissues` (
+CREATE TABLE `tblissues` (
   `id` int(11) NOT NULL,
   `UserEmail` varchar(100) NOT NULL,
   `Issue` varchar(100) NOT NULL,
   `Description` mediumtext NOT NULL,
-  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `AdminRemark` mediumtext,
-  `AdminremarkDate` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblissues`
---
-
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `AdminRemark` mediumtext DEFAULT NULL,
+  `AdminremarkDate` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblpages`
+-- Cấu trúc bảng cho bảng `tblpages`
 --
 
-CREATE TABLE IF NOT EXISTS `tblpages` (
+CREATE TABLE `tblpages` (
   `id` int(11) NOT NULL,
   `type` varchar(255) NOT NULL DEFAULT '',
   `detail` longtext NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tblpages`
+-- Đang đổ dữ liệu cho bảng `tblpages`
 --
 
 INSERT INTO `tblpages` (`id`, `type`, `detail`) VALUES
-(1, 'terms', '<P align=justify><FONT size=2><STRONG><FONT color=#990000>(1) ACCEPTANCE OF TERMS</FONT><BR><BR></STRONG>Welcome to Yahoo! India. 1Yahoo Web Services India Private Limited Yahoo", "we" or "us" as the case may be) provides the Service (defined below) to you, subject to the following Terms of Service ("TOS"), which may be updated by us from time to time without notice to you. You can review the most current version of the TOS at any time at: <A href="http://in.docs.yahoo.com/info/terms/">http://in.docs.yahoo.com/info/terms/</A>. In addition, when using particular Yahoo services or third party services, you and Yahoo shall be subject to any posted guidelines or rules applicable to such services which may be posted from time to time. All such guidelines or rules, which maybe subject to change, are hereby incorporated by reference into the TOS. In most cases the guides and rules are specific to a particular part of the Service and will assist you in applying the TOS to that part, but to the extent of any inconsistency between the TOS and any guide or rule, the TOS will prevail. We may also offer other services from time to time that are governed by different Terms of Services, in which case the TOS do not apply to such other services if and to the extent expressly excluded by such different Terms of Services. Yahoo also may offer other services from time to time that are governed by different Terms of Services. These TOS do not apply to such other services that are governed by different Terms of Service. </FONT></P>\r\n<P align=justify><FONT size=2>Welcome to Yahoo! India. Yahoo Web Services India Private Limited Yahoo", "we" or "us" as the case may be) provides the Service (defined below) to you, subject to the following Terms of Service ("TOS"), which may be updated by us from time to time without notice to you. You can review the most current version of the TOS at any time at: </FONT><A href="http://in.docs.yahoo.com/info/terms/"><FONT size=2>http://in.docs.yahoo.com/info/terms/</FONT></A><FONT size=2>. In addition, when using particular Yahoo services or third party services, you and Yahoo shall be subject to any posted guidelines or rules applicable to such services which may be posted from time to time. All such guidelines or rules, which maybe subject to change, are hereby incorporated by reference into the TOS. In most cases the guides and rules are specific to a particular part of the Service and will assist you in applying the TOS to that part, but to the extent of any inconsistency between the TOS and any guide or rule, the TOS will prevail. We may also offer other services from time to time that are governed by different Terms of Services, in which case the TOS do not apply to such other services if and to the extent expressly excluded by such different Terms of Services. Yahoo also may offer other services from time to time that are governed by different Terms of Services. These TOS do not apply to such other services that are governed by different Terms of Service. </FONT></P>\r\n<P align=justify><FONT size=2>Welcome to Yahoo! India. Yahoo Web Services India Private Limited Yahoo", "we" or "us" as the case may be) provides the Service (defined below) to you, subject to the following Terms of Service ("TOS"), which may be updated by us from time to time without notice to you. You can review the most current version of the TOS at any time at: </FONT><A href="http://in.docs.yahoo.com/info/terms/"><FONT size=2>http://in.docs.yahoo.com/info/terms/</FONT></A><FONT size=2>. In addition, when using particular Yahoo services or third party services, you and Yahoo shall be subject to any posted guidelines or rules applicable to such services which may be posted from time to time. All such guidelines or rules, which maybe subject to change, are hereby incorporated by reference into the TOS. In most cases the guides and rules are specific to a particular part of the Service and will assist you in applying the TOS to that part, but to the extent of any inconsistency between the TOS and any guide or rule, the TOS will prevail. We may also offer other services from time to time that are governed by different Terms of Services, in which case the TOS do not apply to such other services if and to the extent expressly excluded by such different Terms of Services. Yahoo also may offer other services from time to time that are governed by different Terms of Services. These TOS do not apply to such other services that are governed by different Terms of Service. </FONT></P>'),
-(2, 'privacy', '<span style="color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat</span>'),
-(3, 'aboutus', '										<span style="color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;">Test test</span>'),
-(11, 'contact', '										<span style="color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;">Address------Test</span>');
+(1, 'terms', '										<p align=\"justify\"><br></p>\r\n										'),
+(2, 'privacy', '<span style=\"color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat</span>'),
+(3, 'aboutus', '										<div style=\"text-align: justify;\"><br></div>\r\n										'),
+(11, 'contact', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbltourpackages`
+-- Cấu trúc bảng cho bảng `tbltourpackages`
 --
 
-CREATE TABLE IF NOT EXISTS `tbltourpackages` (
+CREATE TABLE `tbltourpackages` (
   `PackageId` int(11) NOT NULL,
   `PackageName` varchar(200) NOT NULL,
   `PackageType` varchar(150) NOT NULL,
@@ -145,78 +133,84 @@ CREATE TABLE IF NOT EXISTS `tbltourpackages` (
   `PackageFetures` varchar(255) NOT NULL,
   `PackageDetails` mediumtext NOT NULL,
   `PackageImage` varchar(100) NOT NULL,
-  `Creationdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `Creationdate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbltourpackages`
+-- Đang đổ dữ liệu cho bảng `tbltourpackages`
 --
 
+INSERT INTO `tbltourpackages` (`PackageId`, `PackageName`, `PackageType`, `PackageLocation`, `PackagePrice`, `PackageFetures`, `PackageDetails`, `PackageImage`, `Creationdate`, `UpdationDate`) VALUES
+(7, 'Tour du l?ch h?i phòng', 'Couple', 'Hihi', 123, '123', '1231313', 'angelababy-JPG_680x0.jpg', '2022-06-12 08:17:33', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblusers`
+-- Cấu trúc bảng cho bảng `tblusers`
 --
 
-CREATE TABLE IF NOT EXISTS `tblusers` (
+CREATE TABLE `tblusers` (
   `id` int(11) NOT NULL,
   `FullName` varchar(100) NOT NULL,
   `MobileNumber` char(10) NOT NULL,
   `EmailId` varchar(70) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `RegDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tblusers`
+-- Đang đổ dữ liệu cho bảng `tblusers`
 --
 
+INSERT INTO `tblusers` (`id`, `FullName`, `MobileNumber`, `EmailId`, `Password`, `RegDate`, `UpdationDate`) VALUES
+(12, 'Chau', '0379032830', 'nobitachauphong@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2022-06-12 08:12:29', '0000-00-00 00:00:00'),
+(13, '1111', '111', 'chau.pham@platform.inc', '29ee8b0ac8ade22e7d5631eac3696a75', '2022-06-12 12:31:46', '0000-00-00 00:00:00'),
+(14, '123', '123', '123@gmail.com', '202cb962ac59075b964b07152d234b70', '2022-06-12 12:45:12', '0000-00-00 00:00:00');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `admin`
+-- Chỉ mục cho bảng `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblbooking`
+-- Chỉ mục cho bảng `tblbooking`
 --
 ALTER TABLE `tblbooking`
   ADD PRIMARY KEY (`BookingId`);
 
 --
--- Indexes for table `tblenquiry`
+-- Chỉ mục cho bảng `tblenquiry`
 --
 ALTER TABLE `tblenquiry`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblissues`
+-- Chỉ mục cho bảng `tblissues`
 --
 ALTER TABLE `tblissues`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblpages`
+-- Chỉ mục cho bảng `tblpages`
 --
 ALTER TABLE `tblpages`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbltourpackages`
+-- Chỉ mục cho bảng `tbltourpackages`
 --
 ALTER TABLE `tbltourpackages`
   ADD PRIMARY KEY (`PackageId`);
 
 --
--- Indexes for table `tblusers`
+-- Chỉ mục cho bảng `tblusers`
 --
 ALTER TABLE `tblusers`
   ADD PRIMARY KEY (`id`),
@@ -224,44 +218,52 @@ ALTER TABLE `tblusers`
   ADD KEY `EmailId_2` (`EmailId`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT cho bảng `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
--- AUTO_INCREMENT for table `tblbooking`
+-- AUTO_INCREMENT cho bảng `tblbooking`
 --
 ALTER TABLE `tblbooking`
-  MODIFY `BookingId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `BookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
--- AUTO_INCREMENT for table `tblenquiry`
+-- AUTO_INCREMENT cho bảng `tblenquiry`
 --
 ALTER TABLE `tblenquiry`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
--- AUTO_INCREMENT for table `tblissues`
+-- AUTO_INCREMENT cho bảng `tblissues`
 --
 ALTER TABLE `tblissues`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
--- AUTO_INCREMENT for table `tblpages`
+-- AUTO_INCREMENT cho bảng `tblpages`
 --
 ALTER TABLE `tblpages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
 --
--- AUTO_INCREMENT for table `tbltourpackages`
+-- AUTO_INCREMENT cho bảng `tbltourpackages`
 --
 ALTER TABLE `tbltourpackages`
-  MODIFY `PackageId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `PackageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
--- AUTO_INCREMENT for table `tblusers`
+-- AUTO_INCREMENT cho bảng `tblusers`
 --
 ALTER TABLE `tblusers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
